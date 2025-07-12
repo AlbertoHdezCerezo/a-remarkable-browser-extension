cb39cd87ae66947cd104a290d776b118e1d908fb
Alberto Hernandez Cerezo <pascu216@gmail.com>
2025-07-12 22:03:29 +0200

    fix: Jest configuration to allow running ESM formatted tests.

---
+++ src/lib/utils/http_client/node_http_client.js
@@ -0,0 +1,18 @@
+/**
+ * HTTP client for JS Node.js environments.
+ *
+ * Underneath the library uses the `https` module from Node.js.
+ * This library is compatible with browser-extension environments
+ * but not with browser environments.
+ *
+ * The reMarkable API requires a Node.js HTTP client library to
+ * be used in order to bypass some of the constraints set by their
+ * CORS policy.
+ *
+ * @class
+ */
+export default class NodeHttpClient {
+	static async get() {
+
+	}
+}
