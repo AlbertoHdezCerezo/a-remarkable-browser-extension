00b435940e9138029e72c7389a95700f452d4d26
Alberto Hernandez Cerezo <pascu216@gmail.com>
2025-07-13 18:18:32 +0200

    chore: linter

--- src/lib/file_manager/file_url.js
+++ src/lib/file_manager/file_url.js
@@ -8,49 +8,49 @@
  * @class
  */
 export default class FileUrl {
-	/**
+  /**
 	 * URL to download the file from the reMarkable
 	 * device via its `Storage` API.
 	 *
 	 * @private
 	 */
-	#url
+  #url
 
-	/**
+  /**
 	 * URL expiration date. Once reached, the
 	 * file will no longer be downloadable
 	 * from the associated URL.
 	 *
 	 * @private
 	 */
-	#expiration
+  #expiration
 
-	/**
+  /**
 	 * Given a Hash representing a remarkable API
 	 * file payload, creates a new instance of FileUrl.
 	 */
-	static fromRemarkableFileHash(remarkableFileHash) {
-		return new FileUrl(
-			remarkableFileHash.url,
-			remarkableFileHash.expiration
-		)
-	}
+  static fromRemarkableFileHash (remarkableFileHash) {
+    return new FileUrl(
+      remarkableFileHash.url,
+      remarkableFileHash.expiration
+    )
+  }
 
-	/**
+  /**
 	 * Creates a new instance of FileUrl.
 	 *
 	 * @constructor
 	 * @param {string} url - remarkable API URL to download a file
 	 * @param {Date} expiration - date when the URL expires
 	 */
-	constructor(url, expiration) {
-		this.#url = url
-		this.#expiration = new Date(expiration)
-	}
+  constructor (url, expiration) {
+    this.#url = url
+    this.#expiration = new Date(expiration)
+  }
 
-	get url() { return this.#url }
+  get url () { return this.#url }
 
-	get expiration() { return this.#expiration }
+  get expiration () { return this.#expiration }
 
-	get expired() { return this.#expiration < new Date() }
+  get expired () { return this.#expiration < new Date() }
 }
--- src/lib/utils/http_client/node_http_client.js
+++ src/lib/utils/http_client/node_http_client.js
@@ -12,7 +12,7 @@
  * @class
  */
 export default class NodeHttpClient {
-	static async get() {
+  static async get () {
 
-	}
+  }
 }
