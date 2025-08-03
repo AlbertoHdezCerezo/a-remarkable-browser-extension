d1339430d2ea4e285a013650df4059bd82ddb11e
Alberto Hernandez Cerezo <pascu216@gmail.com>
2025-08-03 18:07:03 +0200

    chore: refactor HashEntry class.

--- src/lib/remarkableApi/internal/schemas/v4/hashEntry.js
+++ src/lib/remarkableApi/internal/schemas/v4/hashEntry.js
@@ -1,40 +1,106 @@
-import {CONFIGURATION} from '../../../configuration.js'
-import FetchBasedHttpClient from '../../../../utils/httpClient/fetchBasedHttpClient.js'
+import {CONFIGURATION} from '../../../configuration'
+import FetchBasedHttpClient from '../../../../utils/httpClient/fetchBasedHttpClient'
 
-export default class HashEntry {
+export class IncompatibleHashEntrySchemaError extends Error {
+	constructor(
+		hashEntryPayload,
+		message = `
+			Hash entry payload is incompatible with the Hash Entry v4 schema.
+			Expected payload to be a string with the following format:
+			<checksum>:<zero>:<fileId>.<fileExtension>:<nestedHashEntriesCount>:<sizeInBytes>
+			Received payload: ${hashEntryPayload}
+		`
+	) {
+		super(message)
+		this.name = 'IncompatibleHashEntrySchemaError'
+	}
+}
+
+export class UnreachableHashEntryContentError extends Error {
+	constructor(
+		originalError,
+		message = `
+			Attempt to fetch hash entry content from reMarkable cloud API failed.
+			Original error message: ${originalError.message}
+		`
+	) {
+		super(message)
+		this.name = 'UnreachableHashEntryContentError'
+		this.stack = `${this.stack}\nCaused by: ${originalError.stack}`
+	}
+}
+
+/**
+ * Represents a reMarkable cloud API hash entry.
+ *
+ * A hash entry is a string composed by a set of
+ * fields separated by a colon (:):
+ *
+ * Example: cd2696e19cdff3c645bf32c67bf625d9fb86208a6bd3ff33e860d76bf09a604d:0:008302bc-c5ba-41be-925b-8567166246e4.content:0:26531
+ *
+ * - checksum: checksum of the content behind the hash entry
+ * - zero: always 0
+ * - file ID & Extension: unique ID of reMarkable cloud file the hash entry is associated with
+ * - Number of nested hash entries: number of sub-hash entries within content behind the hash entry
+ * - Size in bytes: size of the content behind the hash entry in bytes
+ *
+ * There are two kind of hash entries:
+ *
+ * - 	The ones present in the reMarkable cloud API root.
+ * 		Each hash entry represents a file (document, folder,
+ * 		notebook, etc). They have no file extension, and they
+ * 		always have a number of nested hash entries.
+ *
+ * - 	The ones present under a reMarkable cloud API root
+ * 		hash entry. Each hash entry represents a part of the
+ * 		total content of a file. They have a file extension,
+ * 		and they always have a zero as the second field.
+ */
+export class HashEntry {
 	/**
-	 * Hash entry payload. Returned by the reMarkable API
-	 * when fetching a file's hash entries. Each entry represents
-	 * a sub-file or a part of the file.
+	 * Payload of the hash entry.
+	 *
+	 * Example: cd2696e19cdff3c645bf32c67bf625d9fb86208a6bd3ff33e860d76bf09a604d:0:008302bc-c5ba-41be-925b-8567166246e4.content:0:26531
+	 *
+	 * @typedef {string}
 	 */
 	#payload
 
 	/**
-	 * Sub-file hash
+	 * Checksum of the content behind this hash entry.
+	 *
 	 * @typedef {string}
 	 */
-	#hash
+	#checksum
 
 	/**
-	 * Number of sub-hash entries within file represented by hash entry
+	 * Number of hash entries behind this hash entry.
+	 * Set to 0 for file hash entries.
+	 *
 	 * @type {number}
 	 */
 	#nestedHashEntriesCount
 
 	/**
-	 * Size of the file in bytes.
+	 * Size of the content behind this hash entry in bytes.
+	 *
 	 * @type {number}
 	 */
 	#sizeInBytes
 
 	/**
-	 * Unique UUID identifier for the file.
+	 * Unique UUID identifier for the file associated with this hash entry
+	 * (e.g. a document, folder notebook, etc).
+	 *
 	 * @type {string}
 	 */
 	#fileId
 
 	/**
 	 * File extension associated with the hash entry.
+	 * Only present for file hash entries (missing in
+	 * root hash entries).
+	 *
 	 * @type {string}
  	 */
 	#fileExtension
@@ -42,11 +108,15 @@ export default class HashEntry {
 	constructor(hashEntryPayload) {
 		this.#payload = hashEntryPayload
 
-		const [hash, _zero, subFileHash, nestedHashEntriesCount, sizeInBytes] = hashEntryPayload.split(':')
+		const [checksum, _zero, subFileHash, nestedHashEntriesCount, sizeInBytes] = hashEntryPayload.split(':')
+
+		if (!checksum || !_zero || !subFileHash || !nestedHashEntriesCount || !sizeInBytes) {
+			throw new IncompatibleHashEntrySchemaError(hashEntryPayload)
+		}
 
 		const [fileId, fileExtension] = subFileHash.split('.')
 
-		this.#hash = hash
+		this.#checksum = checksum
 		this.#nestedHashEntriesCount = Number(nestedHashEntriesCount)
 		this.#sizeInBytes = Number(sizeInBytes)
 		this.#fileId = fileId
@@ -55,6 +125,7 @@ export default class HashEntry {
 
 	/**
 	 * Returns the hash entry payload.
+	 *
 	 * @returns {string}
 	 */
 	get payload() {
@@ -62,15 +133,17 @@ export default class HashEntry {
 	}
 
 	/**
-	 * Returns the hash of the sub-file.
+	 * Returns the checksum of the content behind this hash entry.
+	 *
 	 * @returns {string}
 	 */
-	get hash() {
-		return this.#hash
+	get checksum() {
+		return this.#checksum
 	}
 
 	/**
-	 * Returns the number of sub-hash entries within the file represented by this hash entry.
+	 * Returns the number of nested hash entries behind this hash entry.
+	 *
 	 * @returns {number}
 	 */
 	get nestedHashEntriesCount() {
@@ -78,7 +151,8 @@ export default class HashEntry {
 	}
 
 	/**
-	 * Returns the size of the file in bytes.
+	 * Returns the size of the content behind this hash entry in bytes.
+	 *
 	 * @returns {number}
 	 */
 	get sizeInBytes() {
@@ -86,7 +160,8 @@ export default class HashEntry {
 	}
 
 	/**
-	 * Returns the unique UUID identifier for the file.
+	 * Returns the unique UUID identifier for the file associated with this hash entry.
+	 *
 	 * @returns {string}
 	 */
 	get fileId() {
@@ -94,7 +169,9 @@ export default class HashEntry {
 	}
 
 	/**
-	 * Returns the file extension associated with the hash entry.
+	 * Returns the file extension for the hash entry.
+	 * If the hash entry is a root entry, this will be null.
+	 *
 	 * @returns {string | null}
 	 */
 	get fileExtension() {
@@ -102,7 +179,8 @@ export default class HashEntry {
 	}
 
 	/**
-	 * Returns true if the hash entry is a root entry, meaning it has no file extension.
+	 * Returns true if the hash entry is a root entry.
+	 *
 	 * @returns {boolean}
 	 */
 	get rootHashEntry() {
@@ -110,34 +188,40 @@ export default class HashEntry {
 	}
 
 	/**
-	 * Returns the URL to fetch the content of the file represented by this hash entry.
+	 * Returns the URL containing the hash entry content.
+	 *
 	 * @returns {string}
 	 */
 	get url() {
-		return CONFIGURATION.endpoints.sync.v3.endpoints.files + this.hash
+		return CONFIGURATION.endpoints.sync.v3.endpoints.files + this.checksum
 	}
 
 	/**
-	 * Fetches the content of the file represented by this hash entry.
+	 * Fetches the content of the hash entry.
+	 *
 	 * @param {Session} session - The session object containing the authentication token.
 	 * @returns {Promise<object|string>}
 	 */
 	async content(session) {
-		const contentResponse =
-			await FetchBasedHttpClient
-				.get(this.url, {'Authorization': `Bearer ${session.token}`})
-
-		const contentRawPayload = await contentResponse.text()
-
-		/**
-		 * A response can either contain a plain text resembling
-		 * a set of hash entries, or a JSON. Hence, we try to parse
-		 * the response as JSON, and if it fails, we return the raw payload.
- 		 */
 		try {
-			return JSON.parse(contentRawPayload)
+			const contentResponse =
+				await FetchBasedHttpClient
+					.get(this.url, {'Authorization': `Bearer ${session.token}`})
+
+			const contentRawPayload = await contentResponse.text()
+
+			/**
+			 * A response can either contain a plain text resembling
+			 * a set of hash entries, or a JSON. Hence, we try to parse
+			 * the response as JSON, and if it fails, we return the raw payload.
+			 */
+			try {
+				return JSON.parse(contentRawPayload)
+			} catch {
+				return contentRawPayload
+			}
 		} catch (error) {
-			return contentRawPayload
+			throw new UnreachableHashEntryContentError(error)
 		}
 	}
 }
--- test/lib/remarkableApi/internal/schemas/v4/hashEntry.test.js
+++ test/lib/remarkableApi/internal/schemas/v4/hashEntry.test.js
@@ -1,7 +1,10 @@
-import {CONFIGURATION} from '../../../../../../src/lib/remarkableApi/configuration.js'
-import HashEntry from '../../../../../../src/lib/remarkableApi/internal/schemas/v4/hashEntry.js'
-import DeviceConnection from '../../../../../../src/lib/remarkableApi/deviceConnection.js'
-import Session from '../../../../../../src/lib/remarkableApi/session.js'
+import {CONFIGURATION} from '../../../../../../src/lib/remarkableApi/configuration'
+import Session from '../../../../../../src/lib/remarkableApi/session'
+import DeviceConnection from '../../../../../../src/lib/remarkableApi/deviceConnection'
+import {
+	HashEntry,
+	IncompatibleHashEntrySchemaError, UnreachableHashEntryContentError
+} from '../../../../../../src/lib/remarkableApi/internal/schemas/v4/hashEntry'
 
 describe('HashEntry', () => {
 	describe('.construct', () => {
@@ -11,7 +14,7 @@ describe('HashEntry', () => {
 			const hashEntry = new HashEntry(hashEntryPayload)
 
 			expect(hashEntry.payload).toBe(hashEntryPayload)
-			expect(hashEntry.hash).toBe('cd2696e19cdff3c645bf32c67bf625d9fb86208a6bd3ff33e860d76bf09a604d')
+			expect(hashEntry.checksum).toBe('cd2696e19cdff3c645bf32c67bf625d9fb86208a6bd3ff33e860d76bf09a604d')
 			expect(hashEntry.nestedHashEntriesCount).toBe(0)
 			expect(hashEntry.sizeInBytes).toBe(26531)
 			expect(hashEntry.fileId).toBe('008302bc-c5ba-41be-925b-8567166246e4')
@@ -24,11 +27,21 @@ describe('HashEntry', () => {
 			const hashEntry = new HashEntry(hashEntryPayload)
 
 			expect(hashEntry.payload).toBe(hashEntryPayload)
-			expect(hashEntry.hash).toBe('883411c7fa93637f63ada401b9fbe06eda8d16363f946dc7f296a05c3b3ba91d')
+			expect(hashEntry.checksum).toBe('883411c7fa93637f63ada401b9fbe06eda8d16363f946dc7f296a05c3b3ba91d')
 			expect(hashEntry.nestedHashEntriesCount).toBe(5)
 			expect(hashEntry.sizeInBytes).toBe(5665759)
 			expect(hashEntry.fileId).toBe('008302bc-c5ba-41be-925b-8567166246e4')
 			expect(hashEntry.fileExtension).toBe(undefined)
+		})
+
+		it('if payload does not resemble a v4 hash entry, throws an error', () => {
+			const hashEntryPayload = 'cd2696e19cdff3c645bf32c67bf625d9fb86208a6bd3ff33e860d76bf09a604d:0:008302bc-c5ba-41be-925b-8567166246e4.content'
+
+			try {
+				new HashEntry(hashEntryPayload)
+			} catch (error) {
+				expect(error).toBeInstanceOf(IncompatibleHashEntrySchemaError)
+			}
 		})
 	})
 
@@ -53,7 +66,7 @@ describe('HashEntry', () => {
 			const hashEntryPayload = 'cd2696e19cdff3c645bf32c67bf625d9fb86208a6bd3ff33e860d76bf09a604d:0:008302bc-c5ba-41be-925b-8567166246e4.content:0:26531'
 			const hashEntry = new HashEntry(hashEntryPayload)
 
-			expect(hashEntry.url).toBe(CONFIGURATION.endpoints.sync.v3.endpoints.files + hashEntry.hash)
+			expect(hashEntry.url).toBe(CONFIGURATION.endpoints.sync.v3.endpoints.files + hashEntry.checksum)
 		})
 	})
 
@@ -78,6 +91,19 @@ describe('HashEntry', () => {
 			const hashEntryContent = await hashEntry.content(session)
 
 			expect(typeof hashEntryContent).toBe('object')
+		})
+
+		it('if hash entry content cannot be fetched, throws an error', async () => {
+			const deviceConnection = new DeviceConnection(global.remarkableDeviceConnectionToken)
+			const session = await Session.from(deviceConnection)
+			const hashEntryPayload = 'thisisnotavalidchecksum:0:0bacf12a-64fa-4fe5-9f28-16a043e8c809.metadata:0:314'
+			const hashEntry = new HashEntry(hashEntryPayload)
+
+			try {
+				await hashEntry.content(session)
+			} catch (error) {
+				expect(error).toBeInstanceOf(UnreachableHashEntryContentError)
+			}
 		})
 	})
 })
