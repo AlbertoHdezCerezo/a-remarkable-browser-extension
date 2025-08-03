51312c9ec783e6357992a4971129b1a9dcb36322
Alberto Hernandez Cerezo <pascu216@gmail.com>
2025-08-03 20:11:36 +0200

    feat: index for schemas.

---
+++ src/lib/remarkableApi/internal/schemas/index.js
@@ -0,0 +1,5 @@
+import { HashEntry as V3HashEntry } from './v3/hashEntry.js'
+import { HashEntries as V4HashEntries } from './v4/hashEntries'
+import {HashEntriesFactory} from './hashEntriesFactory'
+
+export { V3HashEntry, V4HashEntries, HashEntriesFactory }
