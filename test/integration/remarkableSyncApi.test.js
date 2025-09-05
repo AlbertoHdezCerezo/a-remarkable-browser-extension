d01196c728faea022923638623c17be079b3cdd2
Alberto Hernandez Cerezo <pascu216@gmail.com>
2025-09-04 20:11:50 -0500

    progress: remove cache

--- test/integration/remarkableSyncApi.test.js
+++ test/integration/remarkableSyncApi.test.js
@@ -4,17 +4,20 @@ import {setupHttpRecording} from '../helpers/pollyHelper.js'
 import {FileBuffer} from '../../src/lib/remarkableApi'
 import * as RemarkableApi from '../../src/lib/remarkableApi'
 
-describe('reMarkable API integration specs', () => {
+describe('reMarkable Sync API integration specs', () => {
 	setupHttpRecording()
 
-	let root = null
 	let folder = null
 	let pdfFile = null
 	let epubFile = null
 	let session = global.remarkableApiSession
 
-	afterAll(() => {
-	})
+	it('loads reMarkable root folder', async () => {
+		const root = await RemarkableApi.Internal.Sync.Root.fromSession(session)
+
+		expect(root).toBeInstanceOf(RemarkableApi.Internal.Sync.Root)
+		expect(root.hashEntries.hashEntriesList.length).toBeGreaterThan(0)
+	}, 100000000)
 
 	it('creates a PDF file', async () => {
 		const pdfBuffer = fs.readFileSync('./test/fixtures/documents/sample.pdf')
@@ -37,14 +40,9 @@ describe('reMarkable API integration specs', () => {
 		expect(epubFile.metadata.fileName).toBe("a-remarkable-web-browser-sample.epub")
 	}, 100000000)
 
-	it('creates a folder', async () => {
-		// Load Root instance
-		root = await RemarkableApi.Internal.Sync.Root.fromSession(session)
-		expect(root).toBeInstanceOf(RemarkableApi.Internal.Sync.Root)
-
-		// Create a folder
-		folder = await RemarkableApi.Internal.Sync.V3.Folder.create(root, "A sample folder", session)
-		expect(folder).toBeInstanceOf(RemarkableApi.Internal.Sync.V3.Folder)
-		expect(folder.metadata.folderName).toBe("A sample folder")
-	}, 100000000)
+	afterAll(async () => {
+		if(!!folder) { await folder.moveToTrash(session) }
+		if(!!pdfFile) { await pdfFile.moveToTrash(session) }
+		if(!!epubFile) { await epubFile.moveToTrash(session) }
+	})
 })
