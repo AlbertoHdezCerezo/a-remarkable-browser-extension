80a90f3e1b8cec77cd4f9752048cfd0f921070f7
Alberto Hernandez Cerezo <pascu216@gmail.com>
2025-09-20 09:51:30 +0200

    progress: Dashboard has now a sidebar.

--- src/components/popup/Dashboard.jsx
+++ src/components/popup/Dashboard.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import ActionBar, {ActionBarIconButton} from '../common/ActionBar.jsx'
 
 const Sidebar = ({...props}) => {
 	return (
-		<div className="h-full w-16 flex flex-col">
-			a
+		<div className="h-full flex flex-col items-center justify-between p-1.5">
+			<ActionBar>
+				<ActionBarIconButton iconName="FolderIcon" />
+			</ActionBar>
+
+			<ActionBar>
+				<ActionBarIconButton iconName="Cog6ToothIcon" />
+				<ActionBarIconButton iconName="PowerIcon" />
+			</ActionBar>
 		</div>
 	)
 }
@@ -11,11 +19,12 @@ const Sidebar = ({...props}) => {
 const Dashboard = ({...props}) => {
 	return (
 		<div className="w-[60vh] h-[40vh] flex flex-row items-center justify-center overflow-hidden
-										border-4 border-neutral-900 rounded-[30px]">
-			<div className="h-full bg-neutral-900">
+										border-1 border-neutral-900 divide-x-1 divide-neutral-500">
+			<div className="h-full ">
 				<Sidebar />
 			</div>
-			<div className="h-full flex-grow flex flex-row items-center p-6 bg-white">
+
+			<div className="h-full flex-grow flex flex-row items-center p-6">
 
 			</div>
 		</div>
