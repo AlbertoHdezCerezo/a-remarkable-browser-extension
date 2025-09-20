0cdaace0829f7e5b8a9489abd128d8181d7e7e02
Alberto Hernandez Cerezo <pascu216@gmail.com>
2025-09-20 13:00:08 +0200

    progress: BaseInput component.

---
+++ src/components/common/form/BaseInput.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import Base from '../Base.jsx'
+
+export const CONFIGURATION = {
+	base: {
+		className: `
+			font-monospace-body
+			disabled:cursor-not-allowed
+		`
+	},
+	variant: {
+		default: `
+			bg-white border-1 border-neutral-500 text-neutral-900
+			hover:bg-neutral-100 hover:border-neutral-900
+			focus:bg-neutral-100 focus:border-neutral-900 focus:outline-none
+			disabled:text-neutral-500 disabled:bg-neutral-100
+		`,
+		invisible: `
+			bg-neutral-50 border-0 text-neutral-900
+			hover:bg-neutral-100 hover:border-neutral-100
+			focus:bg-neutral-100 focus:outline-none
+			disabled:cursor-not-allowed disabled:text-neutral-500
+		`
+	},
+	size: {
+		medium: `
+			px-3.5 h-8 text-sm
+		`,
+		large: `
+			px-4 h-9 text-base
+		`,
+	}
+}
+
+const BaseInput = (
+	{
+		as = 'input',
+		size = 'medium',
+		variant = 'default',
+		inactive = false,
+		placeholder = '',
+		type = 'text',
+		id = '',
+		name = '',
+		value = '',
+		onChange = null,
+		classNames = '',
+		...props
+	}
+) => {
+	const [inputValue, setInputValue] = React.useState(value)
+
+	const onInputChange = (inputChangeEvent) => {
+		setInputValue(inputChangeEvent.target.value)
+		if (onChange) onChange(inputChangeEvent)
+	}
+
+	const baseInputClassNames = [
+		CONFIGURATION.base.className,
+		CONFIGURATION.variant[variant],
+		CONFIGURATION.size[size],
+		classNames
+	].join(' ')
+
+	return (
+		<Base as={as}
+		      classNames={baseInputClassNames}
+		      data-size={size}
+		      data-variant={variant}
+		      disabled={inactive}
+		      type={type}
+		      id={id}
+		      name={name}
+		      value={inputValue}
+					placeholder={placeholder}
+					onChange={onInputChange}/>
+	)
+}
+
+export default BaseInput
---
+++ src/stories/common/form/BaseInput.stories.js
@@ -0,0 +1,65 @@
+import {CONFIGURATION} from '../../../components/common/form/BaseInput.jsx'
+import BaseInput from '../../../components/common/form/BaseInput.jsx'
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+	title: 'Common/Form/BaseInput',
+	component: BaseInput,
+	parameters: {
+		// Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+		layout: 'centered',
+	},
+	argTypes: {
+		variant: {
+			control: {
+				type: 'radio',
+			},
+			options: Object.keys(CONFIGURATION.variant),
+		},
+		size: {
+			control: {
+				type: 'radio',
+			},
+			options: Object.keys(CONFIGURATION.size),
+		},
+		inactive: {
+			control: {
+				type: 'boolean',
+			},
+		},
+		placeholder: {
+			control: {
+				type: 'text'
+			}
+		},
+		type: {
+			control: {
+				type: 'radio',
+			},
+			options: ['text', 'password', 'email', 'number', 'tel', 'url']
+		},
+		id: {
+			control: {
+				type: 'text'
+			}
+		},
+		name: {
+			control: {
+				type: 'text'
+			}
+		},
+		value: {
+			control: {
+				type: 'text'
+			}
+		},
+	},
+	// This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+	tags: ['autodocs']
+}
+
+export default meta;
+
+const Template = (args) => <BaseInput {...args} />
+
+export const Playground = Template.bind({})
