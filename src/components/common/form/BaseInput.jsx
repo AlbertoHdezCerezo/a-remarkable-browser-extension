553d9f6e1f1fe94878315b81f5d2d72111dcc0b2
Alberto Hernandez Cerezo <pascu216@gmail.com>
2025-09-28 16:06:08 +0200

    progress: Added wrapper to base input component for easier customization.

--- src/components/common/form/BaseInput.jsx
+++ src/components/common/form/BaseInput.jsx
@@ -4,27 +4,28 @@ import Base from '../Base.jsx'
 export const CONFIGURATION = {
 	base: {
 		className: `
-			font-monospace-body
+			flex items-center justify-center font-monospace-body
 			disabled:cursor-not-allowed
+			[&>input]:focus:outline-none
 		`
 	},
 	variant: {
 		default: `
 			bg-white border-1 border-neutral-500 text-neutral-900
 			hover:bg-neutral-100 hover:border-neutral-900
-			focus:bg-neutral-100 focus:border-neutral-900 focus:outline-none
+			focus:bg-neutral-100 focus:border-neutral-900
 			disabled:text-neutral-500 disabled:bg-neutral-100
 		`,
 		invisible: `
 			bg-neutral-50 border-0 text-neutral-900
 			hover:bg-neutral-100 hover:border-neutral-100
-			focus:bg-neutral-100 focus:outline-none
+			focus:bg-neutral-100
 			disabled:cursor-not-allowed disabled:text-neutral-500
 		`
 	},
 	size: {
 		medium: `
-			px-3.5 h-8 text-sm
+			px-3 h-8 text-sm
 		`,
 		large: `
 			px-4 h-9 text-base
@@ -64,19 +65,22 @@ const BaseInput = (
 	].join(' ')
 
 	return (
-		<Base as={as}
-		      classNames={baseInputClassNames}
+		<Base as="div"
 		      data-size={size}
 		      data-variant={variant}
 		      disabled={inactive}
-		      type={type}
-		      id={id}
-		      name={name}
-		      value={inputValue}
-					placeholder={placeholder}
-          maxLength={maxLength}
-          size={maxLength}
-					onChange={onInputChange}/>
+		      classNames={baseInputClassNames}>
+			<Base as={as}
+			      disabled={inactive}
+			      type={type}
+			      id={id}
+			      name={name}
+			      value={inputValue}
+						placeholder={placeholder}
+	          maxLength={maxLength}
+	          size={maxLength}
+						onChange={onInputChange}/>
+		</Base>
 	)
 }
 
